d646d8ad5e3d787532be76db37904c9b960046ea
johnaresgado95 <64903984+johnaresgado95@users.noreply.github.com>
2022-06-02 14:15:04 +0800

    Fizz buzz java
    For interview purposes only

---
+++ fizz.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class myFizzBuzz {
+	public static void main (String[] args) { 
+		int x = 100; 
+
+		for( int i = 1; i <=n; i++) {
+			if (i%15==0) 
+				System.out.println("FizzBuzz");
+			else if (i%5==0)    
+                System.out.println("Buzz");
+			else if (i%3==0)    
+                System.out.println("Fizz");
+			else 
+                System.out.println(i); 
+		}
+	}
+}
